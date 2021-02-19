244b518c46b3df7f37d6f5972148c0f41d730df1
Achyuth <achyuthmanepally@gmail.com>
2021-02-19 15:57:53 +0530

    first commit

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,18 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void mobileTest()
+	{
+		System.out.println("mobileTest");
+	}
+	@Test
+	public void chargeTest()
+	{
+		System.out.println("chargeTest");
+
+	} 
+
+}
