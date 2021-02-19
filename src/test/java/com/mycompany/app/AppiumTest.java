2011bdde9c1e5ec7bf03dff50da6c05ff3bc77b0
Achyuth <achyuthmanepally@gmail.com>
2021-02-19 16:38:35 +0530

    this is from DemoGit

--- src/test/java/com/mycompany/app/AppiumTest.java
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+http://org.eclipse.ui.intro/showPage?id=overviewpackage com.mycompany.app;
 
 import org.testng.annotations.Test;
 
@@ -7,6 +7,8 @@ public class AppiumTest {
 	public void mobileTest()
 	{
 		System.out.println("mobileTest");
+		System.out.println("mobileTest2");
+		System.out.println("mobileTest3");
 	}
 	@Test
 	public void chargeTest()
